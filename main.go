301233cdb2e445e3dedf6f8b5bb07c0ef895dec2
Tacos4brekky2 <123289331+Tacos4brekky2@users.noreply.github.com>
2024-10-18 18:15:01 -0700

    bang

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "net/http"
+    "google.golang.org/grpc"
+    pb "github.com/Tacos4brekky2/gorequestservice/internal/gogrpc"
+)
+
+
+func main() {
+     
+}
---
+++ scripts/proto.sh
@@ -0,0 +1,1 @@
+protoc --go_out=internal/gogrpc --go-grpc_out=internal/gogrpc --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative service.proto
